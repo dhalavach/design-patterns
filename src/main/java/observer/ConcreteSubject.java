e76a9cccd431c07556327d393c2817b173b3e17f
Dzmitry Halavach <kotik1@protonmail.com>
2022-09-12 22:10:44 +0300

    add more design patterns - observer, etc.

---
+++ src/main/java/observer/ConcreteSubject.java
@@ -0,0 +1,12 @@
+package observer;
+
+public class ConcreteSubject {
+    public void registerObserver(){};
+    public void removeObserver(){};
+    public void notifyObservers(){};
+
+    public void getState() {};
+    public void setState() {};
+
+
+}
